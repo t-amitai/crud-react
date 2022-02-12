b8e8a2360185790fecf1838494d46025b2dc5891
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2022-02-12 16:21:47 -0700

    adds webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
